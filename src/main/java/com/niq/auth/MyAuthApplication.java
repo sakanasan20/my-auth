a2ad2ec66e3dcc639c162a555dbff35a77ce2275
Nick Chen <sakanasan20@gmail.com>
2025-05-08 04:12:11 +0800

    Initial commit

---
+++ src/main/java/com/niq/auth/MyAuthApplication.java
@@ -0,0 +1,13 @@
+package com.niq.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/niq/auth/MyAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.niq.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
